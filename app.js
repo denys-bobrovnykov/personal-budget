6b2f77199865cff6d70f48c2fe32212988eefd71
denys-bobrovnykov <62986552+denys-bobrovnykov@users.noreply.github.com>
2020-12-14 15:34:42 +0200

    Get by id route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = 3000;
 
--- routes/envelopes/envelopes.js
+++ routes/envelopes/envelopes.js
@@ -1,23 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
-const envelopes = [{ id: '0', name: 'test', budget: 0 }];
+const envelopes = [
+  { id: '0', name: 'test', budget: 0 },
+  { id: '2', name: 'test2', budget: 2 }
+];
 const totalBudget = {
   amount: 0
 };
 
-router.get('/', (req, res, next) => {
-  return res.json({ envelopes, totalBudget });
+router.get('/', (req, res) => {
+  res.send({ envelopes, totalBudget });
 });
 
-router.post('/', (req, res, next) => {
+router.get('/:id', (req, res) => {
+  console.log(req.params.id);
+  const envelope = envelopes.filter((env) => env.id === req.params.id);
+  if (envelope.length !== 1) {
+    res.status(400).send('Not found');
+  }
+  res.send(envelope[0]);
+});
+
+router.post('/', (req, res) => {
   const { id, name, budget } = req.body;
   if (id && name && budget) {
     envelopes.push(req.body);
     totalBudget.amount += budget;
-    return res.status(200).send(req.body);
+    res.status(200).send(req.body);
   } else {
-    return res.status(400).send('Error incomplete data');
+    res.status(400).send('Error incomplete data');
   }
 });
 
